a40449eca3b4842a24ca01c3ecab9aaee6b24554
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-07-03 16:59:14 +0700

    feat: update waste bank routes to allow access for USER role

--- src/routes/waste-bank.routes.js
+++ src/routes/waste-bank.routes.js
@@ -10,8 +10,8 @@ import roleMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", roleMiddleware("ADMIN"), getAllWasteBanksController);
-router.get("/:id", roleMiddleware("ADMIN"), getWasteBankByIdController);
+router.get("/", roleMiddleware("ADMIN", "USER"), getAllWasteBanksController);
+router.get("/:id", roleMiddleware("ADMIN", "USER"), getWasteBankByIdController);
 router.post("/", roleMiddleware("ADMIN"), createWasteBankController);
 router.patch("/:id", roleMiddleware("ADMIN"), updateWasteBankController);
 router.delete("/:id", roleMiddleware("ADMIN"), deleteWasteBankController);
